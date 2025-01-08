54c7d240afaf5d466b6ddf4da5cad348f0a43380
Emir Kaynar <emirkaynar20004@gmail.com>
2025-01-09 02:33:05 +0300

    midbuild backup

---
+++ Pages/login.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+
+public class LoginModel : PageModel
+{
+    public string? ErrorMessage { get; private set; }
+
+    public void OnPost(string username, string password)
+    {
+        var xmlHelper = new XmlHelper();
+        var user = xmlHelper.GetUserByUsernameAndPassword(username, password);
+
+        if (user != null)
+        {
+            HttpContext.Session.SetInt32("UserId", user.Id); // Kullanıcı ID'sini Session'a kaydet
+            Response.Redirect("/Index");
+        }
+        else
+        {
+            ErrorMessage = "Invalid username or password.";
+        }
+    }
+}
